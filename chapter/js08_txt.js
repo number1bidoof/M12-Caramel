9a30d31acc73bcca89c6b9968c25b0db1697bf83
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-04-25 12:20:28 -0400

    Done and dusted

--- chapter/js08_txt.js
+++ chapter/js08_txt.js
@@ -3,11 +3,11 @@
       Chapter 8
       Chapter case   
 
-      Draw Poker Game using Object Oriented Programming
-      Author: 
-      Date:       
+      Draw Poker Game using Object Oriented Programming      
+      Author: Ayah Abdalla
+      Date: 04/25/2025    
 
-      Filename:       js08.js
+      Filename:  js08.js
  */
 
 window.addEventListener("load", playDrawPoker);
@@ -22,8 +22,27 @@ function playDrawPoker() {
    let betSelection = document.getElementById("bet");
    let bankBox = document.getElementById("bank");
    let cardImages = document.querySelectorAll("img.cardImg");
+
+   // Set initial bank and bet values
+   pokerGame.currentBank = 500;
+   pokerGame.currentBet = 25;
+   //Create a deck of shuffled cards
+   let myDeck = new pokerDeck();
+   myDeck.shuffle();
+   //Create an empty poker hand object
+   let myHand = new pokerHand(5);
+   //Display the current bank value
+   bankBox.value = pokerGame.currentBank;
+   //Get a new deck is there less than 10 cards left?
+   if (myDeck.cards.length < 10) {
+      myDeck = new pokerDeck();
+      myDeck.shuffle();
+   }
+   // Change the bet when the selection changes
+   betSelection.onchange = function() {
+      pokerGame.currentBet = parseInt(this.value);
+      }
     
-   
       dealButton.addEventListener("click", function() {
       if (pokerGame.currentBank >= pokerGame.currentBet) {
          // Enable the Draw and Stand buttons after the initial deal
@@ -32,8 +51,27 @@ function playDrawPoker() {
          drawButton.disabled = false;       // Turn on the Draw button
          standButton.disabled = false;      // Turn on the Stand Button
          statusBox.textContent = "";        // Erase any status messages
-      }  
-
+         bankBox.value = pokerGame.placeBet();
+         
+      } else {
+         statusBox.textContent = "Insufficient Funds"
+      }
+         //Deal 5 cards from the deck to the hand
+         myDeck.dealTo(myHand);
+         for (let i = 0; i < cardImages.length; i++) {
+            cardImages[i].src = myHand.cards[i].cardImage();
+            //Flip the card images when clicked
+            cardImages[i].onclick = function() {
+               if (this.src.includes("cardback.png")) {
+                  //show the front of the card
+                  this.src = myHand.cards[i].cardImage();
+               } else {
+                  //show the back of the card
+                  this.src = "cardback.png";
+               }
+            }
+   }
+      
    });
    
    
@@ -43,11 +81,22 @@ function playDrawPoker() {
       betSelection.disabled = false;      // Turn on the Bet Selection list
       drawButton.disabled = true;         // Turn off the Draw button
       standButton.disabled = true;        // Turn off the Stand Button
-      
 
+      //Replace cards marked to be discarded
+      for (let i = 0; i <cardImages.length; i++) {
+         if (cardImages[i].src.includes("cardback.png")) {
+            //replace the card and its image on the table
+            myHand.replaceCard(i, myDeck);
+            cardImages[i].src = myHand.cards[i].cardImage();
+         }
+      }
 
+      // Evaluate the hand drawn by user
+      statusBox.textContent = myHand.getHandValue();
+
+      //Update the bank value
+      bankBox.value = pokerGame.payBet(statusBox.textContent);
    });
-   
     
    standButton.addEventListener("click", function() {
       // Enable the Deal and Bet options when the player chooses to stand with their hand 
@@ -56,7 +105,10 @@ function playDrawPoker() {
       drawButton.disabled = true;         // Turn off the Draw button
       standButton.disabled = true;        // Turn off the Stand Button  
 
-    
+      // Evaluate the hand drawn by user
+      statusBox.textContent = myHand.getHandValue();
+      //Update the bank value
+      bankBox.value = pokerGame.payBet(statusBox.textContent);    
    });
    
    
--- chapter/objects_txt.js
+++ chapter/objects_txt.js
@@ -5,42 +5,88 @@
       Chapter case   
 
       Custom Objects Used in Poker Games
-      Author: 
-      Date:       
+      Author: Ayah Abdalla
+      Date: 04/25/2025    
 
       Filename:       objects.js
  */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      let pokerGame = {
+         currentBank: null,
+         currentBet: null,
+         placeBet: function() {
+            this.currentBank -= this.currentBet;
+            return this.currentBank;
+         },
+         payBet: function(type) {
+            let pay = 0;
+            switch(type) {
+               case "Royal Flush": pay = 250; break;
+               case "Straight Flush": pay = 50; break;
+               case "Four of a Kind": pay = 25; break;
+               case "Full House": pay = 9; break;
+               case "Flush": pay = 6; break;
+               case "Straight": pay = 4; break;
+               case "Three of a Kind": pay = 3; break;
+               case "Two Pair": pay = 2; break;
+               case "Jacks or Better": pay = 1; break;
+            }
+            this.currentBank += pay*this.currentBet;
+            return this.currentBet
+         }
+      };
+      
+      //Constructor Function for poker cards
+      function pokerCard(cardSuit, cardRank) {
+         this.suit = cardSuit;
+         this.rank = cardRank;
+      }
+      
+      //Method to reference the image source file for a card
+      pokerCard.prototype.cardImage = function() {
+         return this.rank + "_" + this.suit + ".png";
+      };
+      
+      // Constructor Function for poker decks
+      function pokerDeck() {
+         //List card suits and ranks
+         let suits = ["clubs","diamonds","hearts","spades"];
+         let ranks = ["2","3","4","5","6","7","8","9","10",
+                      "jack","queen","king","ace"];
+         this.cards = []
+      
+         //Add a card for each combo of suit and rank
+         for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 13; j++) {
+               //Add a pokercard object
+               this.cards.push(new pokerCard(suits[i], ranks[j]));
+            }
+         }
+         
+         //Method to randomly sort the cards in the deck
+         this.shuffle = function() {
+            this.cards.sort(function() {
+               return 0.5 - Math.random();
+            });
+         };
+      
+         //Method to deal cards from the deck into a hand
+      
+         this.dealTo = function(pokerHand) {
+            let cardsDealt = pokerHand.cards.length;
+            pokerHand.cards = this.cards.splice(0, cardsDealt);
+         }
+      };
+      
+      //Constructor function for poker hands
+      function pokerHand(handLength) {
+         this.cards = new Array(handLength);
+      }
+      
+      //Method to replace a card in a hand with a card from the deck
+      pokerHand.prototype.replaceCard = function(index, pokerDeck) {
+         this.cards[index] = pokerDeck.cards.shift();
+      };
    
    /* ------------------------------------------------+
    | The handType() function returns a text string of |
